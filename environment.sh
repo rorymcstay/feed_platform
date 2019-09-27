bdc794073c73677a1d769704682e219e8ca448d1
Rory McStay <rory@rorymcstay.com>
2019-09-27 23:23:37 +0100

    Dockerfile task to initialise database

--- environment.sh
+++ environment.sh
@@ -1,3 +1,9 @@
 export IMAGE_REPOSITORY=064106913348.dkr.ecr.us-west-2.amazonaws.com
 export PROJECT_NAME=feed
 export GIT_CLONE_URL=ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos
+
+export MONGO_HOST=localhost
+export MONGO_PORT=20717
+
+export DATABASE_HOST=localhost
+export DATABASE_PORT=5432
