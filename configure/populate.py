e77c51e543099cdd8144fa28ccdd45a9d0226c7f
Rory McStay <rory@rorymcstay.com>
2019-10-09 20:14:43 +0100

    syncing

--- configure/populate.py
+++ configure/populate.py
@@ -1,15 +1,22 @@
 import json
 import os
 import sys
+import logging
 
 if sys.argv[1] is None:
-    print("usage python populate.py <file_path>")
+    print("usage python populate.py <file_path> <out_file_name>")
     sys.exit()
 
 if "_template.json" not in sys.argv[1]:
     print("usage: filename should have _template.json in it")
 
 file = sys.argv[1]
+
+def replaceEnvVar(string):
+    replacement = string.replace("$", "").format(**os.environ)
+    if replacement==string:
+        logging.info("Error replacing {}".format(string))
+    return replacement
 
 
 def replaceIterList(lis):
@@ -20,7 +27,7 @@ def replaceIterList(lis):
         elif isinstance(item, list):
             new.append(replaceIterList(item))
         else:
-            new.append(str(item).replace("${", "{").format(**os.environ))
+            new.append(replaceEnvVar(str(item)))
     return new
 
 
@@ -33,15 +40,17 @@ def replaceIter(dic):
         if isinstance(dic[key], list):
             new.update({key: replaceIterList(dic[key])})
         if isinstance(dic[key], str) and "$" in dic[key]:
-            new.update({key: dic[key].replace("$", "").format(**os.environ)})
+            new.update({key: replaceEnvVar(dic[key])})
+        print(json.dumps(new))
     return new
 
-
 if __name__ == '__main__':
+    variables = dict(**os.environ)
+
     with open(file) as fileString:
         fileString = fileString.read()
         dic = json.loads(fileString)
         populated = replaceIter(dic)
-
+    print(json.dumps(populated))
     with open(sys.argv[2], 'w') as outfile:
         outfile.write(json.dumps(populated))
--- environment.sh
+++ environment.sh
@@ -5,7 +5,7 @@ export GIT_CLONE_URL=ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos
 
 export MONGO_HOST=localhost
 export MONGO_PORT=20717
-
+export DEPLOYMENT_ROOT=~/app/feed
 export DATABASE_HOST=localhost
 export DATABASE_PORT=5432
 
