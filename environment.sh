a7bb2d4fdfbc6160cd58a099bafa7bef81f29258
Rory McStay <rory@rorymcstay.com>
2019-10-27 14:52:04 +0000

    settings

--- environment.sh
+++ environment.sh
@@ -1,5 +1,6 @@
 export IMAGE_REPOSITORY=064106913348.dkr.ecr.us-west-2.amazonaws.com
 export PROJECT_NAME=feed
+export SOURCE_DIR=$HOME/dev/feed
 
 export GIT_CLONE_URL=ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos
 
