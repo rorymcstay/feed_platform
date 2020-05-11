3429a4a096c028797cefacc2a9b0349b30324882
Rory McStay <rory@rorymcstay.com>
2020-05-11 20:23:33 +0100

    publish if release changes

--- scripts/publish-if-release-tag.sh
+++ scripts/publish-if-release-tag.sh
@@ -8,10 +8,13 @@ git checkout $CODEBUILD_RESOLVED_SOURCE_VERSION
 IMAGE_NAME=$PROJECT_NAME/$COMPONENT
 echo "git describe --exact-match --tags $(git log -n1 --pretty='%h')"
 tag=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
+echo "Current source tag is ${tag}"
+
+sem_ver_regex="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+
 
 IS_RELEASE=1
-echo $tag | grep -E -q "^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$" \
-    || IS_RELEASE=0
+echo $tag | grep -E -q $sem_ver_regex || IS_RELEASE=0
 
 if [ $IS_RELEASE -eq "1" ]; then
     echo "publishing docker image version $tag";
