7a7ce14a5d201703385d19dbb374c3a1426d4cf5
Rory McStay <rory@rorymcstay.com>
2020-05-11 22:51:14 +0100

    manifest file and profiles

--- scripts/source_me.sh
+++ scripts/source_me.sh
@@ -1,19 +1,18 @@
 # project details
-export DEPLOYMENT_ROOT=$HOME/app/feed
-export SOURCE_DIR=$HOME/dev/feed
+
 export PROJECT_NAME=feed
 
 # sdlc
 export DEFAULT_REGION=us-west-2
+export IMAGE_REPOSITORY=064106913348.dkr.ecr.us-west-2.amazonaws.com
 export GIT_CLONE_URL=ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos
 
 
 # docker file helpers
 alias dc="docker-compose -f ${DEPLOYMENT_COMPOSE} " 
-alias dc-services="docker-compose -f ${SERVICE_COMPOSE}" # 
+alias dc-services="docker-compose -f ${SERVICE_COMPOSE} " # 
 
-alias gitinfoall="for com in $(ls $SOURCE_DIR); do cd $SOURCE_DIR/$com; echo \"$i\"; git status; cd $SOURCE_DIR; done"
-alias gitpushall="for com in $(ls $SOURCE_DIR); do cd $SOURCE_DIR/$com; echo \"### $i ###\"; git push; cd $SOURCE_DIR; done"
+
 
 
 
