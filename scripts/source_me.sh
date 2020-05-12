2671bcbff10c466df6ddbc4e0386887b2c8ea3a4
Rory McStay <rory@rorymcstay.com>
2020-05-12 01:07:37 +0100

    working profiles and updating feed admin

--- scripts/source_me.sh
+++ scripts/source_me.sh
@@ -1,5 +1,4 @@
 # project details
-
 export PROJECT_NAME=feed
 
 # sdlc
@@ -7,12 +6,7 @@ export DEFAULT_REGION=us-west-2
 export IMAGE_REPOSITORY=064106913348.dkr.ecr.us-west-2.amazonaws.com
 export GIT_CLONE_URL=ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos
 
-
 # docker file helpers
 alias dc="docker-compose -f ${DEPLOYMENT_COMPOSE} " 
 alias dc-services="docker-compose -f ${SERVICE_COMPOSE} " # 
-
-
-
-
 
