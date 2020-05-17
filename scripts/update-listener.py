b19397fe46bda3b40cfa03ec962877265f3fc258
Rory McStay <rory@rorymcstay.com>
2020-05-17 14:18:52 +0000

    small changes on gateway

--- scripts/update-listener.py
+++ scripts/update-listener.py
@@ -3,6 +3,12 @@ from flask import Flask
 import subprocess
 
 
+component_name_overrides = {
+    'routing': 'router'
+}
+
+def get_component_name(name):
+    return component_name_overrides.get(name, name)
 
 class UpdateManager(FlaskView):
 
@@ -11,7 +17,7 @@ class UpdateManager(FlaskView):
         return 'kubectl set image deployment/{name} {name}=064106913348.dkr.ecr.us-west-2.amazonaws.com/feed/{name}:{version}'.format(name=name, version=version).split(' ')
 
     def rolloutImage(self, name, version):
-        command = UpdateManager._updateCommand(name, version)
+        command = UpdateManager._updateCommand(get_component_name(name), version)
         subprocess.Popen(command, bufsize=10)
         return 'ok'
 
