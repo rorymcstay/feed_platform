6c83ff127936fdf104d696320c548f6ecbb53161
Rory McStay <rory@rorymcstay.com>
2020-05-19 23:09:09 +0100

    unit test pull mongo;

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#docker pull selenium/standalone-chrome:3.141.59
-#docker pull mongo:latest
+# docker pull selenium/standalone-chrome:3.141.59
+docker pull mongo:latest
 #docker pull postgres:latest
 source $CODEBUILD_SRC_DIR_platform/etc/profiles/dev.env
 pip install -r requirements.txt
