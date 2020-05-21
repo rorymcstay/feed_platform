bbee17ceb0146b52ffc2ada2cef4b386c9ae4d9e
Rory McStay <rory@rorymcstay.com>
2020-05-22 00:02:51 +0100

    test scripts

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# docker pull selenium/standalone-chrome:3.141.59
+docker pull selenium/standalone-chrome:3.141.59
 docker pull mongo:latest
-#docker pull postgres:latest
+docker pull postgres:latest
+
+docker create network test
+
 source $CODEBUILD_SRC_DIR_platform/etc/profiles/dev.env
 pip install -r requirements.txt
 
