e98d04472ca211f367b10a2b21570f81023e3f74
Rory McStay <rory@rorymcstay.com>
2020-05-24 22:54:58 +0000

    fixiing promote script

--- scripts/promote-to-prod.sh
+++ scripts/promote-to-prod.sh
@@ -16,12 +16,14 @@ SUCCESS=true
 helm upgrade feedmachine $DEPLOYMENT_ROOT/etc/kube/feedmachine --namespace prod --values etc/prod.versions.yaml --values etc/kube/feedmachine/prod-values.yaml || SUCCESS=false
 
 cp $DEPLOYMENT_ROOT/etc/manifest.txt $DEPLOYMENT_ROOT/etc/prod-manifest.txt
-git add etc/manifest.txt etc/prod.versions.yaml etc/prod/manifest.txt
+git add etc/prod.versions.yaml etc/prod-manifest.txt
 
 if [[ $SUCCESS == "false" ]]; then
-    git commit -m 'PROD environment upgrade - FAILURE' && git push
+    git commit -m 'PROD environment upgrade - FAILURE'
+    git push
 else
-    git commit -m 'PROD environment upgrade - SUCCESS' && git push
+    git commit -m 'PROD environment upgrade - SUCCESS'
+    git push
 
     echo success
 fi
